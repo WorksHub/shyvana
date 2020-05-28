f5806c62025d5b289562909a07536b6695c478e1
fricze <andrzej.fricze@gmail.com>
2020-05-28 11:25:52 +0200

    Add repositories

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.getstream.client/stream-java "3.2.3"]])
+                 [io.getstream.client/stream-java "3.2.3"]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]
+                 ["snapshots" {:url "https://clojars.org/repo"
+                               :creds :gpg}]])
