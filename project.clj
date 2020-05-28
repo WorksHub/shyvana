2b14bcc5e7e0ccd407a684fc6cb489f180df0b2f
fricze <andrzej.fricze@gmail.com>
2020-05-28 10:57:33 +0200

    Set project details

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject workshub/shyvana "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Clojure, data-based, wrapper for getstream.io Java API"
+  :url "https://github.com/WorksHub/shyvana"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.getstream.client/stream-java "3.2.3"]]
-  :repl-options {:init-ns shyvana.core})
+                 [io.getstream.client/stream-java "3.2.3"]])
