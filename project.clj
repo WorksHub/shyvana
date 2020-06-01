a2490f97c054f62da84b8653da97c460e6cb74bc
fricze <andrzej.fricze@gmail.com>
2020-06-01 17:48:01 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject workshub/shyvana "0.1.0-SNAPSHOT"
+(defproject workshub/shyvana "0.1.0"
   :description "Clojure, data-based, wrapper for getstream.io Java API"
   :url "https://github.com/WorksHub/shyvana"
   :license {:name "Eclipse Public License"
