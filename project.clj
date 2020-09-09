023a21ec0ba7349da7cd4513ccb9dae0ad68b9e4
jonki <andrzej.fricze@gmail.com>
2020-09-09 16:42:57 +0200

    Version 0.2.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject workshub/shyvana "0.2.8-SNAPSHOT"
+(defproject workshub/shyvana "0.2.8"
   :description "Clojure, data-based, wrapper for getstream.io Java API"
   :url "https://github.com/WorksHub/shyvana"
   :license {:name "Eclipse Public License"
