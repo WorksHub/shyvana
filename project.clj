cd4e15cdf2ea0bd32243de6865b558d171018875
Antony Woods <acron1@gmail.com>
2022-01-10 10:44:37 +0000

    Add 'snapshot' to version number

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject workshub/shyvana "0.2.9"
+(defproject workshub/shyvana "0.2.9-SNAPSHOT"
   :description "Clojure, data-based, wrapper for getstream.io Java API"
   :url "https://github.com/WorksHub/shyvana"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [io.getstream.client/stream-java "3.3.0"]]
-  :repositories [["releases" {:url "https://clojars.org/repo"
+  :repositories [["releases" {:url   "https://clojars.org/repo"
                               :creds :gpg}]
-                 ["snapshots" {:url "https://clojars.org/repo"
+                 ["snapshots" {:url   "https://clojars.org/repo"
                                :creds :gpg}]])
