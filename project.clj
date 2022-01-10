6a9e8b326c50d3809f759b66116b297bd596a90d
Antony Woods <acron1@gmail.com>
2022-01-10 11:20:28 +0000

    Version 0.2.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject workshub/shyvana "0.2.9"
+(defproject workshub/shyvana "0.2.10-SNAPSHOT"
   :description "Clojure, data-based, wrapper for getstream.io Java API"
   :url "https://github.com/WorksHub/shyvana"
   :license {:name "Eclipse Public License"
