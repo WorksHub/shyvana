6b94310a441e90db9c265f5293d07f0ad3f8ce7e
Yanis Urbis <yanis.urbis@gmail.com>
2022-01-10 13:34:03 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject workshub/shyvana "0.3.0-SNAPSHOT"
+(defproject workshub/shyvana "0.2.9"
   :description "Clojure, data-based, wrapper for getstream.io Java API"
   :url "https://github.com/WorksHub/shyvana"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.getstream.client/stream-java "3.2.3"]]
+                 [io.getstream.client/stream-java "3.3.0"]]
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :creds :gpg}]
                  ["snapshots" {:url "https://clojars.org/repo"
